8ccee614dc358beedef744e9bbea5460c1f6123c
246_135 <My_email_adress@gmail.com>
2017-07-17 17:23:26 -0700

    Created new Project Selenium

---
+++ java-example/src/test/java/MyFirstTest.java
@@ -0,0 +1,45 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.elementSelectionStateToBe;
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
+
+/**
+ * Created by Irina on 7/17/17.
+ */
+public class MyFirstTest {
+
+  private WebDriver driver;
+  private WebDriverWait wait;
+
+  @Before
+  public void start() {
+    driver = new ChromeDriver();
+    wait = new WebDriverWait(driver, 10);
+
+  }
+
+  @Test
+  public void testMyFirst() {
+
+    driver.get("http://google.com");
+    driver.findElement(By.name("q")).sendKeys("webdriver");
+    wait.until(visibilityOfElementLocated(By.name("btnG")));
+    driver.findElement(By.name("btnG")).click();
+    wait.until(titleIs("webdriver - Google Search"));
+
+  }
+
+  @After
+  public void stop() {
+    driver.quit();
+    driver = null;
+  }
+
+}
