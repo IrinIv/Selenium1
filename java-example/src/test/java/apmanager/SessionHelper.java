e4cebed45d8a46633108b6d805ef5f9fd5519ad0
Irina <myemail@gmail.com>
2017-08-11 16:19:10 -0700

    Changed remote.properties for Cloud

--- java-example/src/test/java/apmanager/SessionHelper.java
+++ java-example/src/test/java/apmanager/SessionHelper.java
@@ -2,6 +2,7 @@ package apmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * Created by IrinaIv on 7/20/2017.
@@ -14,10 +15,13 @@ public class SessionHelper {
   }
 
   protected void login(String username, String password) {
+    WebDriverWait wait = new WebDriverWait(driver, 10);
     //driver.get("http://localhost/litecart/admin/login.php");
+    driver.findElement(By.name("username")).clear();
     driver.findElement(By.name("username")).sendKeys(username);
     driver.findElement(By.name("username")).click();
+    driver.findElement(By.name("password")).clear();
     driver.findElement(By.name("password")).sendKeys(password);
-    driver.findElement(By.name("login")).click();
+    driver.findElement(By.xpath("//*[@id=\"box-login\"]/form/div[2]/button")).click();
   }
 }
